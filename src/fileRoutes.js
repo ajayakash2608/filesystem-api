96dfdc7419880a8b72f89859952df57042d030b6
ajayakash2608 <130428831+ajayakash2608@users.noreply.github.com>
2024-09-16 15:46:36 +0530

    Update fileRoutes.js

--- src/fileRoutes.js
+++ src/fileRoutes.js
@@ -1,7 +1,7 @@
 // src/routes/fileRoutes.js
 const express = require('express');
 const router = express.Router();
-const fileController = require('E:\\Ajay\\Password-Reset\\file system projec\\file-system-api\\src\\fileController.js');
+const fileController = require('../fileController'); // Adjusted the path to relative path based on the project tree
 
 // Route for creating a file
 router.post('/create-file', fileController.createFile);
