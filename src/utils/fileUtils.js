751d94142d9059c8c820e2c9f0ae791f0fcfb4fb
ajayakash2608 <130428831+ajayakash2608@users.noreply.github.com>
2024-09-16 14:54:58 +0530

    Delete src/utils directory

--- src/utils/fileUtils.js
+++
@@ -1,9 +0,0 @@
-// src/utils/fileUtils.js
-const getCurrentTimestamp = () => new Date().toISOString();
-
-const getFormattedDate = () => {
-  const now = new Date();
-  return `${now.getFullYear()}-${(now.getMonth() + 1).toString().padStart(2, '0')}-${now.getDate().toString().padStart(2, '0')}_${now.getHours().toString().padStart(2, '0')}-${now.getMinutes().toString().padStart(2, '0')}-${now.getSeconds().toString().padStart(2, '0')}`;
-};
-
-module.exports = { getCurrentTimestamp, getFormattedDate };
